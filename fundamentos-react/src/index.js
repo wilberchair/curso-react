49dc5848b277906ec18e3984b6a055c6ab03cc49
wilberchair <wilberchair@gmail.com>
2022-05-05 18:47:02 -0300

    chore(react): montando projeto e estruturando

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+// const el = document.getElementById('root')
+
+// ReactDOM.render('Olá Reactttt', el)
+ReactDOM.render(
+  <div>
+    <strong>Olá React!!!</strong>
+  </div>,
+  document.getElementById('root')
+)
