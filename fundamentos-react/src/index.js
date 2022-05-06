0040e0ce35725a896f441eab9f0b02ffae098a47
wilberchair <wilberchair@gmail.com>
2022-05-06 10:30:12 -0300

    feat(react): finalizando primeiro componente

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,12 +2,15 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import Primeiro from './components/basicos/Primeiro'
+
 // const el = document.getElementById('root')
 
 // ReactDOM.render('Olá Reactttt', el)
 ReactDOM.render(
   <div>
-    <strong>Olá React!!!</strong>
+    {/* <strong>Olá React!!!</strong> */}
+    <Primeiro></Primeiro>
   </div>,
   document.getElementById('root')
 )
