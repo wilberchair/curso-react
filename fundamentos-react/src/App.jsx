63741f047766b7e61296a420004a835877feed70
wilberchair <wilberchair@gmail.com>
2022-05-08 15:47:07 -0300

    chore(react): passando acor do card via parametro e melhorias no layout

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 
 import Card from './components/layout/Card'
@@ -10,14 +11,25 @@ import Primeiro from './components/basicos/Primeiro'
 //poderia deixar parenteses sem nada no parametro ou posso deixar o underline
 //que representa que o parametro nao é importante
 export default _ =>
-  <div id='app'>
+  <div className='App'>
     <h1>Fundamentos React</h1>
 
-    <Card title='Exemplo de card'></Card>
-    
-    <Aleatorio min={10} max={60} />
-    <Fragmento />
-    <ComParametro title='Situação do aluno' aluno='Wilber' nota={5.3} />
-    <ComParametro title='Situação do aluno' aluno='Cristielly' nota={9.9} />
-    <Primeiro></Primeiro>
+    <div className='Cards'>
+      <Card title='#04 - Exemplo de card' color='#0f0'>
+        <Aleatorio min={10} max={60} />
+      </Card>
+      
+      <Card title='#03 - Fragmento' color='#875'>
+        <Fragmento />
+      </Card>
+
+      <Card title='#02 - Com Parâmetro' color='#234'>
+        <ComParametro title='Situação do aluno' aluno='Wilber' nota={5.3} />
+        <ComParametro title='Situação do aluno' aluno='Cristielly' nota={9.9} />
+      </Card>
+      
+      <Card title='#01 - Primeiro Componente' color='#f12'>
+        <Primeiro></Primeiro>
+      </Card>
+    </div>
   </div>
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -2,10 +2,16 @@ import './card.css'
 import React from 'react'
 
 export default props => {
+  
+  const cardStyle = {
+    backgroundColor: props.color || '#f00',
+    borderColor: props.color || '#f00'
+  }
+  
   return (
-    <div className='Card'>
+    <div className='Card' style={ cardStyle }>
       <div className='Title'>{ props.title }</div>
-      <div className='Content'>Conteúdo</div>
+      <div className='Content'>{ props.children }</div>
     </div>
   )
 }
